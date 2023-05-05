0514765b1403f571ea5cbf7b79de2f728fed6748
ntqkha <ntqkha@gmail.com>
2023-05-05 22:38:33 +0700

    xoa comment

--- screens/Home.js
+++ screens/Home.js
@@ -30,8 +30,6 @@ const LineDivider = () => {
 };
 
 
-
-
 const Home = ({ navigation }) => {
   // Dummy Datas
   let profileData = {
@@ -265,128 +263,6 @@ const Home = ({ navigation }) => {
       </View>
     );
   }
-
-  // function renderButtonSection() {
-  //   return (
-  //     <View
-  //       style={{ flex: 1, justifyContent: "center", padding: SIZES.padding }}
-  //     >
-  //       <View
-  //         style={{
-  //           flexDirection: "row",
-  //           height: 70,
-  //           backgroundColor: COLORS.secondary,
-  //           borderRadius: SIZES.radius,
-  //         }}
-  //       >
-  //         {/* Claim */}
-  //         <TouchableOpacity
-  //           style={{ flex: 1 }}
-  //           onPress={() => console.log("Claim")}
-  //         >
-  //           <View
-  //             style={{
-  //               flex: 1,
-  //               flexDirection: "row",
-  //               alignItems: "center",
-  //               justifyContent: "center",
-  //             }}
-  //           >
-  //             <Image
-  //               source={icons.claim_icon}
-  //               resizeMode="contain"
-  //               style={{
-  //                 width: 30,
-  //                 height: 30,
-  //               }}
-  //             />
-  //             <Text
-  //               style={{
-  //                 marginLeft: SIZES.base,
-  //                 ...FONTS.body3,
-  //                 color: COLORS.white,
-  //               }}
-  //             >
-  //               Claim
-  //             </Text>
-  //           </View>
-  //         </TouchableOpacity>
-
-  //         {/* Divider */}
-  //         <LineDivider />
-
-  //         {/* Get Point */}
-  //         <TouchableOpacity
-  //           style={{ flex: 1 }}
-  //           onPress={() => console.log("Get Point")}
-  //         >
-  //           <View
-  //             style={{
-  //               flex: 1,
-  //               flexDirection: "row",
-  //               alignItems: "center",
-  //               justifyContent: "center",
-  //             }}
-  //           >
-  //             <Image
-  //               source={icons.point_icon}
-  //               resizeMode="contain"
-  //               style={{
-  //                 width: 30,
-  //                 height: 30,
-  //               }}
-  //             />
-  //             <Text
-  //               style={{
-  //                 marginLeft: SIZES.base,
-  //                 ...FONTS.body3,
-  //                 color: COLORS.white,
-  //               }}
-  //             >
-  //               Get Point
-  //             </Text>
-  //           </View>
-  //         </TouchableOpacity>
-
-  //         {/* Divider */}
-  //         <LineDivider />
-
-  //         {/* My Card */}
-  //         <TouchableOpacity
-  //           style={{ flex: 1 }}
-  //           onPress={() => console.log("My Card")}
-  //         >
-  //           <View
-  //             style={{
-  //               flex: 1,
-  //               flexDirection: "row",
-  //               alignItems: "center",
-  //               justifyContent: "center",
-  //             }}
-  //           >
-  //             <Image
-  //               source={icons.card_icon}
-  //               resizeMode="contain"
-  //               style={{
-  //                 width: 30,
-  //                 height: 30,
-  //               }}
-  //             />
-  //             <Text
-  //               style={{
-  //                 marginLeft: SIZES.base,
-  //                 ...FONTS.body3,
-  //                 color: COLORS.white,
-  //               }}
-  //             >
-  //               My Card
-  //             </Text>
-  //           </View>
-  //         </TouchableOpacity>
-  //       </View>
-  //     </View>
-  //   );
-  // }
 
   function renderMyBookSection(myBooks) {
     const renderItem = ({ item, index }) => {
